f944db16c25c05ca726bd0c4ca89075363a7a7e8
Max <max@Maxs-MacBook-Pro.local>
2020-02-06 20:35:31 -0500

    Initial Commit

---
+++ Danyas/DanyaLOL.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using CatGame;
+using CatGame.Items;
+
+namespace Danyas
+{
+    public class DanyaLOL : MonoBehaviour
+    {
+        public void Start()
+        {
+            _Player = FindObjectOfType<PlayerController>();
+            _scoring = FindObjectOfType<ScoringSystem>();
+            camera = FindObjectOfType<Camera>();
+            originalScene = SceneManager.GetActiveScene();
+            Cursor.lockState = CursorLockMode.None;
+            if (Cursor.lockState.Equals(CursorLockMode.Locked)) {
+                cursorHidden = true;
+            } else
+            {
+                cursorHidden = false;
+            }
+
+
+        }
+
+        public void OnGUI()
+        {
+            GUI.Box(new Rect(10, 10, Screen.width / 4, Screen.height / 4), "Danyas by Max");
+            if (GUI.Button(new Rect(20, 40, 150, 50), "Break All"))
+            {
+                _Items = FindObjectsOfType<DestructibleItem>();
+                _RItems = FindObjectsOfType<ResetableItem>();
+                foreach (DestructibleItem item in _Items)
+                {
+                    item.Break(10000f);
+                }
+                foreach (ResetableItem item in _RItems)
+                {
+                    item.Lift(100f);
+                    item.transform.position = new Vector3(item.transform.position.x + 2f, item.transform.position.y + 5f);
+                }
+            }
+            if (GUI.Button(new Rect(20, 60, 150, 50), "Reload Danyas"))
+            {
+                Reload();
+            }
+        }
+
+        public void Reload()
+        {
+            _Player = FindObjectOfType<PlayerController>();
+            _scoring = FindObjectOfType<ScoringSystem>();
+            camera = FindObjectOfType<Camera>();
+            originalScene = SceneManager.GetActiveScene();
+            Cursor.lockState = CursorLockMode.None;
+            if (Cursor.lockState.Equals(CursorLockMode.Locked))
+            {
+                cursorHidden = true;
+            }
+            else
+            {
+                cursorHidden = false;
+            }
+        }
+
+        public void Update()
+        {
+            if (Input.GetKey(KeyCode.U))
+            {
+                if (cursorHidden)
+                {
+                    Cursor.lockState = CursorLockMode.None;
+                }
+                else
+                {
+                    Cursor.lockState = CursorLockMode.Locked;
+                }
+            }
+        }
+
+        private PlayerController _Player;
+        private DestructibleItem[] _Items;
+        private ResetableItem[] _RItems;
+        private ScoringSystem _scoring;
+        private Camera camera;
+        private Scene originalScene;
+        private Scene currentScene;
+        private bool cursorHidden;
+    }
+}
---
+++ Danyas/DanyasLoader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Danyas
+{
+    public class DanyasLoader
+    {
+        static void Init()
+        {
+            DanyasLoader.Load = new GameObject();
+            DanyasLoader.Load.AddComponent<DanyaLOL>();
+            UnityEngine.Object.DontDestroyOnLoad(DanyasLoader.Load);
+        }
+
+        static void Unload()
+        {
+            GameObject.Destroy(DanyasLoader.Load);
+        }
+
+        private static GameObject Load;
+    }
+}
